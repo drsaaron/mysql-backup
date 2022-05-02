ed00d09d4a2e88156678d61e88c6811d95062cbf
Scott Aaron <dr_saaron@yahoo.com>
2022-05-02 11:12:08 -0500

    remove old files

--- cleanupOldBackups.sh
+++ cleanupOldBackups.sh
@@ -2,3 +2,11 @@
 
 removeOldFiles.sh -e asc -d 30
 
+lineCount=$(git status --porcelain=v1 | wc -l)
+if [ $lineCount -gt 0 ]
+then
+    versionUpdate.sh -m 'remove old files'
+else
+    echo "no old files removed" 1>&2
+fi
+
